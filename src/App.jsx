320a45eb58763e78ed1ee02becdb0aa318f2ccc4
Sukhjit Singh <127196002+the-sukhsingh@users.noreply.github.com>
2024-02-29 22:26:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Sukhjit Singh",
+  title: "Web App Designer & Content Creator",
+  email: "sukhaji65@gmail.com",
+  gitHub: "the-sukhsingh",
+  instagram: "the_sukhsingh",
+  linkedIn: "sukhjit-singh-ab025825b",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "the_sukhsingh",
+  youTube: "the_sukhsingh",
 };
 
 const primaryColor = "#4E567E";
